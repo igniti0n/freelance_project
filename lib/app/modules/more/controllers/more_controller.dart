e69ed256df8922716984ce03bba071ca7105910e
Sohil Memon <sohil.threat@gmail.com>
2021-02-11 19:03:11 +0530

    Delete Account Bug Fixed

--- lib/app/modules/more/controllers/more_controller.dart
+++ lib/app/modules/more/controllers/more_controller.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test_project_one/app/data/API/api_calls.dart';
 import 'package:test_project_one/app/modules/more/provider/more_provider.dart';
 import 'package:test_project_one/app/routes/app_pages.dart';
 import 'package:test_project_one/app/widgets/colours.dart';
 import 'package:test_project_one/app/widgets/constants.dart';
+import 'package:test_project_one/main.dart';
 
 class MoreController extends GetxController with StateMixin {
   //TODO: Implement MoreController
@@ -25,14 +28,20 @@ class MoreController extends GetxController with StateMixin {
     try {
       MoreProvider moreProvider = MoreProvider();
       var result = await moreProvider.deleteAccount(password: password);
+      String message = result[kMessage] ?? 'User Deleted Successfully';
       change(result, status: RxStatus.success());
       Get.snackbar(
         Strings.SUCCESS,
-        value.message ?? Strings.SOMETHING_WRONG,
+        message,
         duration: Duration(milliseconds: 2000),
         backgroundColor: colour_time,
         colorText: Colors.white,
       );
+
+      final pref = await SharedPreferences.getInstance();
+      pref.remove(kUserData);
+      loginModel = null;
+
       Future.delayed(Duration(milliseconds: 1000), () {
         Get.offAllNamed(AppPages.INITIAL);
       });
